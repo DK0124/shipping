2fa191df9cde9cde6527082c0e493003a1bd4b25
DK0124 <125949908+DK0124@users.noreply.github.com>
2025-07-23 18:44:48 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1810,12 +1810,12 @@
       }
     }
     
-    .bv-label-page .list-item {
-      border-bottom: 1px solid #000 !important;
-    }
-    .bv-label-page .list-item:last-child {
-      border-bottom: none !important;
-    }
+      tr.list-item {
+        border-bottom: 1px solid #000 !important;
+      }
+      tr.list-item:last-child {
+        border-bottom: none !important;
+      }
 
     /* 預設管理優化 */
     .bv-preset-controls {
